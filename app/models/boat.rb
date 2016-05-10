37c0653559a76b683b87cacca41b7f4f5dc37ac8
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-05-10 18:56:51 -0400

    Creating seed files and setting up validation

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,2 +1,7 @@
 class Boat < ActiveRecord::Base
+
+	validates :name, presence: true
+	validates :name, length: {minimum: 3, maximum: 50}
+	validates :name, uniqueness: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Boat.delete_all
+Job.delete_all
+
+10.times do
+	Boat.create(location: Faker::Address.country, user_id: User.all.sample, name: Faker::Company.name, capacity: rand(50..200))
+end 
+
+20.times do
+	boat = Boat.all.sample
+	Job.create(boat_id: boat.id, body: Faker::Lorem.sentence, name: Faker::Company.catch_phrase, cost: rand(1000...5000), origin: Faker::Address.country, destination: Faker::Address.country, containers_reqd: rand(1..boat.capacity))
+end
