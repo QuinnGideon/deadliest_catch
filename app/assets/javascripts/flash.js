a66201074b100d9dfc85410e4a9d88c81cdca1d5
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-05-12 14:56:56 -0400

    Adding jquery for fadeout on alerts

---
+++ app/assets/javascripts/flash.js
@@ -0,0 +1,5 @@
+// This is going to make the flash messgaes fade out
+
+$('.notice').fadeOut(5000);
+
+$('.alert').fadeOut(5000);
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ActiveRecord::Base
 	validates :name, presence: true
 	validates :name, uniqueness: true
+	validates :body, length: {minimum: 50}
 
 	validates :cost, presence: true 
 
